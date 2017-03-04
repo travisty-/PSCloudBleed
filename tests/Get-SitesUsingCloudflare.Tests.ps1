1be30692e3284fd87a2957cbf934f41d3c10feb7
Travis Kinney <travisty-@users.noreply.github.com>
2017-03-03 21:55:39 -0700

    Add Pester tests
    Added some basic Pester tests to ensure that returned objects are of the
    correct type.

--- tests/Get-SitesUsingCloudflare.Tests.ps1
+++ tests/Get-SitesUsingCloudflare.Tests.ps1
@@ -1,9 +1,33 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
+. "$here\..\src\$sut"
 
 Describe "Get-SitesUsingCloudflare" {
-    It "does something useful" {
-        $true | Should Be $false
+    Context "When it produces output." {
+        Mock -CommandName Get-SitesUsingCloudflare -MockWith {
+            $list = [System.Collections.Generic.List[PSObject]]::new()
+            $objects = [char[]](65..90) | ForEach-Object { [PSCustomObject]@{Name = "$_.com"} }
+
+            [void] $list.Add($objects)
+            return $list
+        } -Verifiable
+
+        $sites = Get-SitesUsingCloudflare -Name 'Test'
+
+        It "Should return one or more PSCustomObjects." {
+            $sites | Should BeOfType System.Management.Automation.PSCustomObject
+            Assert-VerifiableMocks
+        }
+    }
+
+    Context "When it produces no output." {
+        Mock -CommandName Get-SitesUsingCloudflare -MockWith {} -Verifiable
+
+        $sites = Get-SitesUsingCloudflare -Name 'Test'
+
+        It "Should return null." {
+            $sites | Should BeNullOrEmpty
+            Assert-VerifiableMocks
+        }
     }
 }
